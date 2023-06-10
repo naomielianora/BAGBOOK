3d6b5f3089d7339eabb3276891c84fa739e4b6ad
naomielianora <naomitanujaya13@gmail.com>
2023-06-10 16:17:14 +0700

    fix database + make some ejs file

--- data/Tubes-BasisData.sql
+++ data/Tubes-BasisData.sql
@@ -10,9 +10,11 @@ CREATE TABLE `User` (
   `password` varchar(35) NOT NULL,
   `nama_lengkap` varchar(35) NOT NULL,
   `email` varchar(35) NOT NULL,
-  `fotoU` blob NOT NULL,
-  `status` int(1) NOT NULL DEFAULT 1
+  `fotoU` blob,
+  `status` int(1) NOT NULL DEFAULT 1,
+  CONSTRAINT `UK_user` UNIQUE (`username`, `email`, `idU`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `Follow` (
   `idU1` int(11) NOT NULL,
@@ -22,117 +24,109 @@ CREATE TABLE `Follow` (
 CREATE TABLE `Review` (
   `idR` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `teks` varchar(255) NOT NULL,
-  `nilai` int(1) NOT NULL,
-  `idU` varchar(11) NOT NULL,
-  `idT` varchar(11) NOT NULL
+  `nilai` int(11) NOT NULL,
+  `idU` int(11) NOT NULL,
+  `idT` int(11) NOT NULL,
+  CONSTRAINT `UK_Review` UNIQUE (`idR`, `idU`, `idT`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Nilai_Review` (
-  `nilai` int(1) NOT NULL,
+  `nilai_review` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `arti_nilai` varchar(35) NOT NULL,
+  CONSTRAINT `UK_nilai_review` UNIQUE (`nilai_review`, `arti_nilai`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Tas` (
   `idT` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `namaT` varchar(20) NOT NULL,
-  `fotoT` blob NOT NULL,
+  `fotoT` blob,
   `panjang` float(11) NOT NULL,
   `lebar` float(11) NOT NULL,
   `tinggi` float(11) NOT NULL,
   `warna` varchar(20) NOT NULL,
   `idSubK` int(11) NOT NULL,
   `idM` int(11) NOT NULL,
-  `idDes` int(11) DEFAULT NULL
+  `idDes` int(11) DEFAULT NULL,
+  CONSTRAINT `UK_tas` UNIQUE (`idT`, `idSubK`, `idM`, `idDes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Kategori` (
   `idK` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `namaK` varchar(20) NOT NULL
+  `namaK` varchar(20) NOT NULL,
+  CONSTRAINT `UK_kategori` UNIQUE (`idK`, `namaK`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Sub-kategori`(
-	`isSubK` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`idSubK` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `namaSubK` varchar(20) NOT NULL,
-    `idK` int(11) NOT NULL
+    `idK` int(11) NOT NULL,
+    CONSTRAINT `UK_sub_kategori` UNIQUE (`idSubK`, `namaSubK`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Merk` (
   `idM` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `namaM` varchar(20) NOT NULL
+  `namaM` varchar(20) NOT NULL,
+  CONSTRAINT `UK_merk` UNIQUE (`idM`, `namaM`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `Designer` (
   `idDes` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `namaDes` varchar(20) NOT NULL
+  `namaDes` varchar(20) NOT NULL,
+  CONSTRAINT `UK_des` UNIQUE (`idDes`, `namaDes`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `User`
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`);
-
-ALTER TABLE `Follow`
-  ADD CONSTRAINT `idU1` FOREIGN KEY (`idU1`) REFERENCES `User` (`idU`),
-  ADD CONSTRAINT `idU2` FOREIGN KEY (`idU2`) REFERENCES `User` (`idU`);
-
-ALTER TABLE `Review`
-  ADD CONSTRAINT `nilai` FOREIGN KEY (`nilai`) REFERENCES `Nilai_Review` (`nilai`),
-  ADD CONSTRAINT `idU` FOREIGN KEY (`idU`) REFERENCES `User` (`idU`),
-  ADD CONSTRAINT `idT` FOREIGN KEY (`idT`) REFERENCES `Tas` (`idT`);
-
-ALTER TABLE `Nilai_Review`
-  ADD UNIQUE KEY `nilai` (`nilai`),
-  ADD UNIQUE KEY `arti_nilai` (`arti_nilai`);
-
-ALTER TABLE `Tas`
-  ADD CONSTRAINT `idSubK` FOREIGN KEY (`idSubK`) REFERENCES `Sub-kategori` (`idU`),
-  ADD CONSTRAINT `idM` FOREIGN KEY (`idM`) REFERENCES `Merk` (`idU`),
-  ADD CONSTRAINT `idDes` FOREIGN KEY (`idDes`) REFERENCES `Designer` (`idDes`);
-
-ALTER TABLE `Kategori`
-  ADD UNIQUE KEY `namaK` (`namaK`);
-
-ALTER TABLE `Sub-kategori`
-  ADD CONSTRAINT `idK` FOREIGN KEY (`idK`) REFERENCES `Kategori` (`idK`);
-
-ALTER TABLE `Sub-kategori`
-  ADD UNIQUE KEY `namaSubK` (`namaSubK`);
-
-ALTER TABLE `Merk`
-  ADD UNIQUE KEY `namaM` (`namaM`);
-
-ALTER TABLE `Designer`
-  ADD UNIQUE KEY `namaD` (`namaD`);
-
 INSERT INTO `User` (`username`, `password`, `nama_lengkap`, `email`) VALUES
-(`naomi_elianora`, `naomi123`, `Naomi Elianora`, `naomielianora@gmail.com`),
-(`ghabrielg`, `ghabriel123`, `Ghabriel Galyndeo`, `ghabriel@gmail.com`);
+('naomi_elianora', 'naomi123', 'Naomi Elianora', 'naomielianora@gmail.com'),
+('ghabrielg', 'ghabs123', 'Ghabriel Galyndeo', 'ghabrielg@gmail.com');
 
---idU1 mengfollow idU2
 INSERT INTO `Follow` (`idU1`, `idU2`) VALUES
-(1,2);
+('1','2');
+
+INSERT INTO `Nilai_Review` (`nilai_review`, `arti_nilai`) VALUES
+('1', 'sangat jelek'),
+('2', 'jelek'),
+('3', 'biasa saja'),
+('4', 'bagus'),
+('5', 'sangat bagus');
+
+INSERT INTO `Kategori` (`namaK`) VALUES
+('Handbag');
+
+INSERT INTO `Sub-kategori` (`namaSubK`, `idK`) VALUES
+('Bucket', '1');
+
+INSERT INTO `Merk` (`namaM`) VALUES
+('Coach');
+
+
+INSERT INTO `Tas` (`namaT`, `panjang`, `lebar`, `tinggi`, `warna`, `idSubK`, `idM`) VALUES
+('DAKOTA', '34.5', '15', '29','Brown', '1', '1');
 
 INSERT INTO `Review` (`teks`, `nilai`, `idU`, `idT`) VALUES
-(`bagus banget!`, 4, 1, 1),
-(`lumayanlah`, 3, 2, 1);
+('bagus banget!', '4', '1', '1'),
+('lumayanlah', '3', '2', '1');
 
-INSERT INTO `Nilai_Review` (`nilai`, `arti_nilai`) VALUES
-(1, `sangat jelek`),
-(2, `jelek`),
-(3, `biasa saja`),
-(4, `bagus`),
-(5, `sangat bagus`);
 
-INSERT INTO `Tas` (`nama`, `panjang`, `lebar`, `tinggi`, `warna`, `idSubK`, `idM`, `idDes`) VALUES
-(`DAKOTA`, 34.5, 15, 29, `Brown`, 1, 1, 1);
 
-INSERT INTO `Kategori` (`nama`) VALUES
-(`Handbag`);
+ALTER TABLE `Review`
+  ADD CONSTRAINT `fk_nilai_review` FOREIGN KEY (`nilai`) REFERENCES `Nilai_Review` (`nilai_review`),
+  ADD CONSTRAINT `fk_user_review` FOREIGN KEY (`idU`) REFERENCES `User` (`idU`),
+  ADD CONSTRAINT `fk_tas_review` FOREIGN KEY (`idT`) REFERENCES `Tas` (`idT`);
 
-INSERT INTO `Sub-kategori` (`nama`, `idK`) VALUES
-(`Bucket`, 1);
 
-INSERT INTO `Merk` (`nama`) VALUES
-(`Coach`);
+ALTER TABLE `Follow`
+  ADD CONSTRAINT `fk_following` FOREIGN KEY (`idU1`) REFERENCES `User` (`idU`),
+  ADD CONSTRAINT `fk_followed` FOREIGN KEY (`idU2`) REFERENCES `User` (`idU`);
+
+ALTER TABLE `Sub-kategori`
+  ADD CONSTRAINT `fk_sk_k` FOREIGN KEY (`idK`) REFERENCES `Kategori` (`idK`);
+
+
+ALTER TABLE `Tas`
+  ADD CONSTRAINT `fk_sk_tas` FOREIGN KEY (`idSubK`) REFERENCES `Sub-kategori` (`idSubK`),
+  ADD CONSTRAINT `fk_merk_tas` FOREIGN KEY (`idM`) REFERENCES `Merk` (`idM`),
+  ADD CONSTRAINT `fk_des_tas` FOREIGN KEY (`idDes`) REFERENCES `Designer` (`idDes`);
+
 
 
 
